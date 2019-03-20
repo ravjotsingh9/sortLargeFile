4903e498798b680d73b69b6484a917b5163629b9
Ravjot Singh <ravjotsingh9@yahoo.com>
2019-03-20 02:24:55 -0400

    trying indentation fix with visual studio code

--- src/main/java/sortservice/merge/FilesMergeProcessor.java
+++ src/main/java/sortservice/merge/FilesMergeProcessor.java
@@ -43,7 +43,6 @@ public class FilesMergeProcessor implements Tasklet, StepExecutionListener {
     public FilesMergeProcessor(){
         this.maxRecords = this.DEFAULT_MAX_ROWS_READ;
         this.readers = new ArrayList<FlatFileItemReader<String>>();
-     
     }
     
     /**
@@ -54,14 +53,13 @@ public class FilesMergeProcessor implements Tasklet, StepExecutionListener {
     public FlatFileItemReader<String> fileReader( Resource in) throws Exception {
 
         return new FlatFileItemReaderBuilder<String>().name("file-reader").resource(in)
-                .lineMapper(new LineMapper<String>(){
-                
-                    @Override
-                    public String mapLine(String line, int lineNumber) throws Exception {
-                        return line;
-                    }
-                })
-                .build();
+            .lineMapper(new LineMapper<String>(){
+                @Override
+                public String mapLine(String line, int lineNumber) throws Exception {
+                    return line;
+                }
+            })
+            .build();
     }
 
     /**
@@ -71,22 +69,20 @@ public class FilesMergeProcessor implements Tasklet, StepExecutionListener {
     */
     public FlatFileItemWriter<String> fileWriter( Resource resource) {
         return new FlatFileItemWriterBuilder<String>()
-                .name("file-writer")
-                .resource(resource)
-                .lineAggregator(new LineAggregator<String>(){
-                
-                    @Override
-                    public String aggregate(String item) {
-                        return item;
-                    }
-                })
-                .build();
+            .name("file-writer")
+            .resource(resource)
+            .lineAggregator(new LineAggregator<String>(){  
+                @Override
+                public String aggregate(String item) {
+                    return item;
+                }
+            })
+            .build();
     }
 
     @Override
     public synchronized void beforeStep(StepExecution stepExecution) {
         logger.debug("Lines Processor initialized.");
-
         
         // Read the list of intermediate
         File folder = new File(Config.DESTINATION_LOCATION);
@@ -94,99 +90,94 @@ public class FilesMergeProcessor implements Tasklet, StepExecutionListener {
 
         // Go over the list of files and add them to readers obj
         for (int i = 0; i < listOfFiles.length; i++) {
-		if (listOfFiles[i].isFile()) {
-                	FlatFileItemReader<String> fReader;
-                	try {
-                    		fReader = fileReader(new FileSystemResource(listOfFiles[i]));
-                    		readers.add(fReader);
-                	} catch (Exception e) {
-                    		// TODO Auto-generated catch block
-                    		e.printStackTrace();
-                	}
-            	}	 
-	}
-
+            if (listOfFiles[i].isFile()) {
+                FlatFileItemReader<String> fReader;
+                try {
+                    fReader = fileReader(new FileSystemResource(listOfFiles[i]));
+                    readers.add(fReader);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }	 
+        }
         // Create writer for the output file
-        this.writer= fileWriter(new FileSystemResource(Config.DESTINATION_FILE_LOCATION));
-       
+        this.writer= fileWriter(new FileSystemResource(Config.DESTINATION_FILE_LOCATION)); 
     }
 
      @Override
      public synchronized RepeatStatus execute(StepContribution stepContribution, 
-       ChunkContext chunkContext) throws Exception {
-
+        ChunkContext chunkContext) throws Exception {
         // Execution context
         ExecutionContext context = new ExecutionContext();
         
         // Read first line from each file into the memory
-	logger.info("Reading first line of each intermediate file");
+        logger.info("Reading first line of each intermediate file");
         Map<FlatFileItemReader<String>, String> firstLines = new HashMap<FlatFileItemReader<String>, String>();
         for (FlatFileItemReader<String> r : readers) {
-		r.open(context);
-		firstLines.put(r, r.read());
-	}
-
-
+            r.open(context);
+            firstLines.put(r, r.read());
+        }
+        
         // Open output file for writing
-	writer.setTransactional(false);
-	writer.open(context);
-
+        writer.setTransactional(false);
+        writer.open(context);
+        
         // List to contain all the write content before dumping to file
         List<String> writeList = new ArrayList<String>();
         
         // Create a local copy of all reader
-	List<FlatFileItemReader<String>> readers = new ArrayList<FlatFileItemReader<String>>();
+        List<FlatFileItemReader<String>> readers = new ArrayList<FlatFileItemReader<String>>();
         readers.addAll(this.readers);
         
-
         // Variable to avoid writing a word if it is already written
         String prevWinner = "";
+        
+        while (readers.size() > 0) {
+            int numReaders = readers.size();
 
-	while (readers.size() > 0) {
-		int numReaders = readers.size();
+            // Assume first reader is winner
+            FlatFileItemReader<String> winningReader = readers.get(0);
+            String winner = firstLines.get(winningReader);
             
-		// Assume first reader is winner
-		FlatFileItemReader<String> winningReader = readers.get(0);
-            	String winner = firstLines.get(winningReader);
+            // Traverse over other readers to see which one is real winner
+            for (int i = 1; i < numReaders; i++) {
+                String current = firstLines.get(readers.get(i));
+                if (winner.compareTo(current) > 0) {
+                    winner = current;
+                    winningReader = readers.get(i);
+                }
+            }
             
-            	// Traverse over other readers to see which one is real winner
-		for (int i = 1; i < numReaders; i++) {
-			String current = firstLines.get(readers.get(i));
-			if (winner.compareTo(current) > 0) {
-				winner = current;
-				winningReader = readers.get(i);
-			}
-		}
-            
-            	// If only, the current winner word is different from previous then only add it to writelist
-		if(winner.compareTo(prevWinner) != 0){  
-                	if (writeList.size() < maxRecords) {
-                    		writeList.add(winner);   
-                	} else {
-				writer.write(writeList);
-				writeList = new ArrayList<String>();
-				System.gc();
-				writeList.add(winner);
-                	}
-                	prevWinner = winner;
-           	}  
-		// fetch next line for the winner reader
-		String next = winningReader.read();
-		if (next == null) {
-			winningReader.close();
-			readers.remove(winningReader);
-		} else {
-			firstLines.put(winningReader, next);
-		}
-	}
-		
-	// Write remaining to output
-	if (writeList.size() > 0) {
-		writer.write(writeList);
-	}
-		
-	// Close writer
-	writer.close();
+            // If only, the current winner word is different from previous then only add it to writelist
+            if(winner.compareTo(prevWinner) != 0){  
+                if (writeList.size() < maxRecords) {
+                    writeList.add(winner);   
+                } else {
+                    writer.write(writeList);
+                    writeList = new ArrayList<String>();
+                    System.gc();
+                    writeList.add(winner);
+                }
+                prevWinner = winner;
+            }  
+            // fetch next line for the winner reader
+            String next = winningReader.read();
+            if (next == null) {
+                winningReader.close();
+                readers.remove(winningReader);
+            } else {
+                firstLines.put(winningReader, next);
+            }
+        }
+        
+        // Write remaining to output
+        if (writeList.size() > 0) {
+            writer.write(writeList);
+        }
+        
+        // Close writer
+        writer.close();
 		
         return RepeatStatus.FINISHED;
        }
