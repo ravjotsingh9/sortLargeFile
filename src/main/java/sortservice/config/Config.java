6f7a597dacdde3bb023f85a49ffa7e1713e7baed
Ravjot Singh <ravjotsingh9@yahoo.com>
2019-03-20 01:46:58 -0400

    Fixed indentation in config

--- src/main/java/sortservice/config/Config.java
+++ src/main/java/sortservice/config/Config.java
@@ -2,25 +2,24 @@ package sortservice.config;
 
 public class Config {
     /**
-	 * Number of newline separated lines to be read at once into memory
-     * Be careful here, too big choice can cause thrashing, hence bad performance
-	 */
+    * Number of newline separated lines to be read at once into memory
+    * Be careful here, too big choice can cause thrashing, hence bad performance
+    */
     public static final Integer CHUNK_SIZE_IN_TOTAL_LINES = 2500000;
 
-
     /**
-	 * Text input file
-     * 
-	 */
+    * Text input file location
+    * 
+    */
     public static final String SOURCE_FILE_PATH = "./src/main/resources/big.txt";
 
     /**
-	 * Output file location. The directory path must exist.
-	 */
+    * Output file location. The directory path must exist.
+    */
     public static final String DESTINATION_FILE_LOCATION = "./output/out.full";
 
     /**
-	 * Intermediate file location.The directory path must exist.
-	 */
+    * Intermediate file location.The directory path must exist.
+    */
     public static final String DESTINATION_LOCATION = "./output/";
 }
